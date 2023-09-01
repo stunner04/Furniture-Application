9843e9b1b2b70fb6654ec0f0f4ecd592cd28d33f
stunner04 <yashv0347@gmail.com>
2023-09-02 00:19:13 +0530

    Special ViewModel MainFragment

--- app/src/main/java/com/example/furnitureapp/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/furnitureapp/fragments/categories/MainCategoryFragment.kt
@@ -1,7 +1,36 @@
 package com.example.furnitureapp.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.furnitureapp.R
+import com.example.furnitureapp.adapters.SpecialProductsAdapter
+import com.example.furnitureapp.databinding.FragmentMainCategoryBinding
 
 class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
+    private lateinit var binding : FragmentMainCategoryBinding
+    private lateinit var specialProductAdapter : SpecialProductsAdapter
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentMainCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpSpecialProductsRV()
+    }
+
+    private fun setUpSpecialProductsRV() {
+        specialProductAdapter = SpecialProductsAdapter()
+        binding.rvSpecialProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            adapter=specialProductAdapter
+        }
+    }
 }
---
+++ app/src/main/java/com/example/furnitureapp/viemodel/MainCategoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.furnitureapp.viemodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MainCategoryViewModel @Inject constructor(): ViewModel() {
+
+}
