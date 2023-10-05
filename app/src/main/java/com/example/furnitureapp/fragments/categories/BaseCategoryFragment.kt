fa2ae6667c4dbb5e9c7f917cff7f43099df143eb
stunner04 <yashv0347@gmail.com>
2023-10-06 02:08:32 +0530

    Handling the Paging( Vertical RV + Horizontal RV ) & Loading for the child fragment in Base Category Fragment.

--- app/src/main/java/com/example/furnitureapp/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/furnitureapp/fragments/categories/BaseCategoryFragment.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.furnitureapp.R
 import com.example.furnitureapp.adapters.BestProductAdapter
 import com.example.furnitureapp.databinding.FragmentBaseCategoryBinding
 
 open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
     private lateinit var binding: FragmentBaseCategoryBinding
-    private lateinit var offerAdapter:BestProductAdapter
-    private lateinit var bestProductAdapter: BestProductAdapter
+
+    protected val offerAdapter: BestProductAdapter by lazy { BestProductAdapter() }
+    protected val bestProductAdapter: BestProductAdapter by lazy { BestProductAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,19 +33,51 @@ open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
 
         setupOfferRV()
         setupBestProductRV()
+
+        binding.rvOffer.addOnScrollListener(object :
+            RecyclerView.OnScrollListener() {  // handles paging or infinite Scrolling for horizontal RV
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (!recyclerView.canScrollVertically(1) && dx != 0) {
+                    onOfferProductPagingRequest()
+                }
+            }
+        })
+        // handles paging or infinite Scrolling for vertical RV
+        binding.nestedScrollBaseCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, _, scrollY, _, _ ->
+            if (v.getChildAt(0).bottom <= v.height + scrollY) {
+                onBestProductPagingRequest()
+            }
+        })
     }
+
+    fun showOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideOfferLoading(){
+        binding.offerProductsProgressBar.visibility = View.GONE
+    }
+    fun showBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.VISIBLE
+    }
+    fun hideBestProductsLoading(){
+        binding.bestProductsProgressBar.visibility = View.GONE
+    }
+    open fun onOfferProductPagingRequest() {}
+    open fun onBestProductPagingRequest() {}
     private fun setupBestProductRV() {
-        bestProductAdapter = BestProductAdapter()
         binding.rvBestProducts.apply {
-            layoutManager = GridLayoutManager(requireContext(),2,GridLayoutManager.VERTICAL,false)
-            adapter=bestProductAdapter
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductAdapter
         }
     }
+
     private fun setupOfferRV() {
-        offerAdapter = BestProductAdapter()
         binding.rvOffer.apply {
-            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
-            adapter=offerAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
         }
     }
 }
