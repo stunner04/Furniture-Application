a9146f6caf3b9b449a3a999f27740e494aaecb4c
stunner04 <yashv0347@gmail.com>
2023-10-15 18:10:58 +0530

    Making the Product class parcelable and adding arguments for ProductDetailsFragment.kt

--- app/src/main/java/com/example/furnitureapp/data/Product.kt
+++ app/src/main/java/com/example/furnitureapp/data/Product.kt
@@ -1,5 +1,9 @@
 package com.example.furnitureapp.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Product(
     val id: String,
     val name: String,
@@ -10,7 +14,11 @@ data class Product(
     val colors: List<Int>? = null,
     val sizes: List<String>? = null,
     val images: List<String>
-)
+) : Parcelable
 {
+    /*
+    * A secondary constructor with default values, allowing you to create instances of the Product class
+    *  with default property values when no arguments are provided.
+    * */
     constructor() :  this("0","","",0f, images = emptyList())
 }
