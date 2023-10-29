c8bc6f878e87c86b7d5fafc71c3287e64f507ef9
stunner04 <yashv0347@gmail.com>
2023-10-29 23:16:21 +0530

    Common methods to be used for multiple classes for inserting the document in cart collection.

---
+++ app/src/main/java/com/example/furnitureapp/firebase/FirebaseCommon.kt
@@ -0,0 +1,51 @@
+package com.example.furnitureapp.firebase
+
+import com.example.furnitureapp.data.CartProduct
+import com.example.furnitureapp.data.CartProduct.Companion.copy
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Transaction
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.lang.Exception
+
+class FirebaseCommon(
+    private val firestore: FirebaseFirestore,
+    val auth: FirebaseAuth
+) {
+    val cartCollection = firestore.collection("user").document(auth.uid!!).collection("cart")
+
+    fun addProductToCart(cartProduct: CartProduct, onResult: (CartProduct?, Exception?) -> Unit) {
+        /*
+        * cartCollection is a reference to a Firestore collection. The document() function without any arguments generates a new document
+        * reference with a unique ID. Then, the set() method is used to set the data of this new document with the cartProduct object.
+        * This essentially adds the cartProduct to the Firestore database as a new document.
+        */
+        cartCollection.document().set(cartProduct)
+            .addOnSuccessListener {
+                onResult(cartProduct, null)
+            }
+            .addOnFailureListener {
+                onResult(null, it)
+            }
+    }
+
+    fun increaseQuantity(documentId: String, onResult: (String?, Exception?) -> Unit) {
+        firestore.runTransaction { transaction ->
+            val documentRef = cartCollection.document(documentId)
+            val document = transaction.get(documentRef)
+            val productObject = document.toObject(CartProduct::class.java)
+            productObject?.let { cartProduct ->
+                val newQuantity = cartProduct.quantity + 1
+                val newProductObject = cartProduct.copy(newQuantity)
+                transaction.set(documentRef, newProductObject)
+            }
+        }
+            .addOnSuccessListener {
+                onResult(documentId, null)
+            }
+            .addOnFailureListener {
+                onResult(null, it)
+            }
+    }
+}
