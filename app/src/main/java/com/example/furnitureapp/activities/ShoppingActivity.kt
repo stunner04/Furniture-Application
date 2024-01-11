5dd54569ae778c216f6ededac163d7253ffc2009
stunner04 <yashv0347@gmail.com>
2024-01-11 19:20:00 +0530

    Added a badge to Cart option to display the number of products in the cart.

--- app/src/main/java/com/example/furnitureapp/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/furnitureapp/activities/ShoppingActivity.kt
@@ -2,28 +2,57 @@ package com.example.furnitureapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.furnitureapp.R
 import com.example.furnitureapp.databinding.ActivityShoppingBinding
+import com.example.furnitureapp.util.Resource
+import com.example.furnitureapp.viemodel.CartViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
-   private val binding by lazy {
-       ActivityShoppingBinding.inflate(layoutInflater)
-   }
+    private val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+    val viewModel by viewModels<CartViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.shoppingNavHostFragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.shoppingNavHostFragment) as NavHostFragment
 
         val navController = navHostFragment.navController
 
         binding.shoppingBottomNavigation.setupWithNavController(navController)
+
+        // Showing the badge on the Cart Fragment of the products added in the cart by using the cartProducts state from cartViewModel
+        lifecycleScope.launchWhenStarted {
+            viewModel.cartProducts.collectLatest {
+                when (it) {
+                    is Resource.Success -> {
+                        val count = it.data?.size ?: 0
+                        val bottomNavigation =
+                            findViewById<BottomNavigationView>(R.id.shoppingBottomNavigation)
+                        bottomNavigation.getOrCreateBadge(R.id.cartFragment).apply {
+                            number = count
+                            backgroundColor = resources.getColor(R.color.g_blue)
+                        }
+                    }
+
+                    else -> {
+                        Unit
+                    }
+                }
+            }
+        }
     }
 }
