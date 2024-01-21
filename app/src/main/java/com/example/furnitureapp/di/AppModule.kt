e5307463002d6982b59ada08a53d34fc991b4bc1
stunner04 <yashv0347@gmail.com>
2024-01-21 17:38:03 +0530

    Required dependency for firebase storage and coroutines. Also setup DI for storage.

--- app/src/main/java/com/example/furnitureapp/di/AppModule.kt
+++ app/src/main/java/com/example/furnitureapp/di/AppModule.kt
@@ -8,6 +8,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,4 +36,7 @@ object AppModule {
     fun provideFirebaseCommon(firebaseAuth: FirebaseAuth, fireStore: FirebaseFirestore) =
         FirebaseCommon(fireStore, firebaseAuth)
 
+    @Provides
+    @Singleton
+    fun providestorage() =  FirebaseStorage.getInstance().reference
 }
