e66c766d413f079ab87c9ed7610859d5110c8084
stunner04 <yashv0347@gmail.com>
2024-01-27 01:47:22 +0530

    Retrieving the user data from firestore.Also logout functionality of firebase is used for logout from user account.

---
+++ app/src/main/java/com/example/furnitureapp/viemodel/ProfileViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.furnitureapp.viemodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.furnitureapp.data.User
+import com.example.furnitureapp.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore,
+    private val auth: FirebaseAuth
+) : ViewModel() {
+
+    private val _user = MutableStateFlow<Resource<User>>(Resource.Unspecified())
+    val user = _user.asStateFlow()
+
+    init {
+        getUser()
+    }
+
+    // Retrieve the user collection from the fireStore and added the snapshot as if the user updates it profile,user get update
+    fun getUser() {
+        viewModelScope.launch {
+            _user.emit(Resource.Loading())
+        }
+        firestore.collection("user").document(auth.uid!!).addSnapshotListener { value, error ->
+            if (error != null) {
+                viewModelScope.launch {
+                    _user.emit(Resource.Error(error.message.toString()))
+                }
+            } else {
+                val user = value?.toObject(User::class.java)
+                user?.let {
+                    viewModelScope.launch {
+                        _user.emit(Resource.Success(user))
+                    }
+                }
+
+            }
+        }
+    }
+
+    // LogOut from the user account
+    fun logOutUser() {
+        auth.signOut()
+    }
+}
